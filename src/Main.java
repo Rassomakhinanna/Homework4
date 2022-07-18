b65146bb66619cc22c9490f072f176c7202020b7
annarassomahina <pupkinppppp@gmail.com>
2022-07-18 15:36:52 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 1
+        int clientOS = 0;
+        if ( clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if ( clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задание 2
+    }
+}
